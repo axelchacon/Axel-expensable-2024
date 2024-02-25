f8f6aa05cdbdc4d866bf4fdc6d8d79cc2611d265
Axel Diego Chacón Pérez <axel.chacon@pucp.edu.pe>
2024-02-25 16:33:29 -0500

    paso 2.3 : añadiendo a index.js y sessions-services código para que capture el error correctamente si hay algún error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 ///Por parte de Axel, se comenzó el 24/02/2024. Van 2 días.
 import { login } from "./scripts/services/sessions-service.js";
 
-login({ email: "axelchacon@mail.co", password: "123456" }).then((user) =>
-  console.log(user)
-);
+login({ email: "axelchacon@mail.co", password: "123456" })
+  .then((user) => console.log(user))
+  .catch((error) => console.log(error));
 console.log("Hola");
