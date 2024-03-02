69acb672627934e4614fff275ca07054b9663ab5
Axel Diego Chacón Pérez <axel.chacon@pucp.edu.pe>
2024-03-01 19:10:37 -0500

    revisar error

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+import { tokenKey } from "./scripts/config.js";
 import DOMHandler from "./scripts/dom_handler.js";
 import LoginPage from "./scripts/pages/login-page.js";
 import HomePage from "./scripts/pages/home-page.js";
-DOMHandler.load(HomePage);
+import { login } from "./scripts/services/sessions-service.js";
+import { getUser } from "./scripts/services/user-service.js";
+// DOMHandler.load(HomePage);
+async function init() {
+  try {
+    const token = sessionStorage.getItem(tokenKey);
+    if (!token) return DOMHandler.load(LoginPage);
+    //Home//////
+    const user = await getUser();
+    console.log(user);
+    DOMHandler.load(HomePage);
+  } catch (error) {
+    console.log(error);
+    sessionStorage.removeItem(tokenKey);
+    DOMHandler.load(LoginPage);
+  }
+}
+login({ email: "axelchacon@mail.co", password: "123456" });
+init();
