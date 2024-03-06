aa20fd6398c0d324677a31bd0fe3e122b93165ee
Axel Diego Chacón Pérez <axel.chacon@pucp.edu.pe>
2024-03-05 20:32:52 -0500

    añadiendo el home page algo y que se muestre los clicks en la consola

--- scripts/pages/home-page.js
+++ scripts/pages/home-page.js
@@ -1,8 +1,30 @@
 import STORE from "../store.js";
 
+function listenNavigation() {
+  const navigation = document.querySelector(".js-navigation");
+  navigation.addEventListener("click", (event) => {
+    event.preventDefault();
+    const link = event.target.closest("[data-tab]");
+    if (!link) return;
+    console.log("LLegó Bitch");
+    // console.log(event.target.dataset.tab); "event.target" puede tomar  "link"
+    console.log(link.dataset.tab);
+  });
+}
+
 function render() {
   console.log(STORE);
-  return `<h1>Home Page</h1> <p>Esto es una frase</p>`;
+  return `
+  <main class ="section">
+     <section class ="container">
+        <h3 class="heading heading--lg text-center mb-2">Expensable</h3>
+        <div class="flex justify-between mb-8 js-navigation">
+           <a href="#" class="button button--subtle js-nav-link" data-tab="expense">Expense</a> 
+           <a href="#" class="button button--subtle js-nav-link" data-tab="income">Income</a>
+           <a href="#" class="button button--subtle js-nav-link" data-tab="profile">Profile</a>
+        </div>
+     </section>
+  <main>`;
 }
 
 const HomePage = {
@@ -10,7 +32,9 @@ const HomePage = {
     return render();
   },
 
-  addListeners() {},
+  addListeners() {
+    listenNavigation();
+  },
 };
 
 export default HomePage;
--- scripts/store.js
+++ scripts/store.js
@@ -1,11 +1,19 @@
 import { getCategories } from "./services/categories-service.js";
 async function fetchCategories() {
   const categories = await getCategories();
-  this.categories = categories;
+  // this.categories = categories;
+  this.income = categories.filter(
+    (category) => category.transaction_type === "income"
+  );
+  this.expense = categories.filter(
+    (category) => category.transaction_type === "expense"
+  );
 }
 const STORE = {
   user: null,
-  categories: [],
+  // categories: [],
+  income: [],
+  expense: [],
   //   loginError: null,
   fetchCategories, //Se puede hacer para los async
 };
